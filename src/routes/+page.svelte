fa543c9fd3a90b6def41f1323e3b1c97d0ff04cb
Edlâneo Júlio <edlanioj@gmail.com>
2023-07-15 11:47:16 +0100

    feat: add tailwind css configs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex gap-4 flex-col h-screen items-center justify-center antialiased">
+  <h1 class="text-4xl font-bold">Welcome to <span class="bg-gradient-to-r from-rose-400 to-orange-300 text-transparent bg-clip-text">SvelteKit</span></h1>
+  <p>Visit <a class="underline text-orange-600 mt-2" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
